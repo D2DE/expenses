b8c88681115401968a315cd0f7dee498c6275a58
D2DE <yessahab@gmail.com>
2024-05-12 20:09:23 +0530

    Update script.js

--- script.js
+++ script.js
@@ -22,6 +22,21 @@ document.addEventListener('DOMContentLoaded', function() {
             cell.textContent = value;
         });
     }
+    function googleLogin() {
+  var provider = new firebase.auth.GoogleAuthProvider();
+  firebase.auth().signInWithPopup(provider).then(function(result) {
+    // This gives you a Google Access Token. You can use it to access the Google API.
+    var token = result.credential.accessToken;
+    // The signed-in user info.
+    var user = result.user;
+    // ...
+  }).catch(function(error) {
+    // Handle Errors here.
+    var errorCode = error.code;
+    var errorMessage = error.message;
+    // ...
+  });
+}
 
     function generatePDF(tableId, reportTitle, fileName) {
         const { jsPDF } = window.jspdf;
