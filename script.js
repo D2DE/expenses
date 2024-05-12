fd1bf38a9b4d629f1dcdbc09fc43c60e21f073f3
D2DE <yessahab@gmail.com>
2024-05-12 19:23:10 +0530

    Update script.js

--- script.js
+++ script.js
@@ -43,6 +43,13 @@ document.addEventListener('DOMContentLoaded', function() {
         // This feature is not implemented yet
         alert("Save as Excel feature coming soon!");
     }
+    function onSignIn(googleUser) {
+    var profile = googleUser.getBasicProfile();
+    console.log('ID: ' + profile.getId()); // Do not send to your backend! Use an ID token instead.
+    console.log('Name: ' + profile.getName());
+    console.log('Image URL: ' + profile.getImageUrl());
+    console.log('Email: ' + profile.getEmail()); // This is null if the 'email' scope is not present.
+}
 
     if (expenseForm) {
         expenseForm.addEventListener('submit', function(event) {
